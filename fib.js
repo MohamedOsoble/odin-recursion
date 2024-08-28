6f2bb2370f452b9fd7e2d120b339843284d60e03
MohamedOsoble <108898435+MohamedOsoble@users.noreply.github.com>
2024-08-28 18:56:30 +0100

    add comments

--- fib.js
+++ fib.js
@@ -1,32 +1,37 @@
 function fibs(number){
+
+    // initialize array to store numbers
     fibArray = [];
     for(i = 0; i < number; i++){
         if(i < 2){
+
+            // Set base Case
             fibArray.push(i);
         }
         else{
+            // Add the two previous numbers in the array
             fibArray.push(fibArray[i-1] + fibArray[i-2])
         }
     }
     return fibArray;
 }
 
-// console.log(fibs(5));
-// console.log(fibs(8));
-// console.log(fibs(10));
-
 
 function fibsRec(number){
     if(number === 1){
+
+        // Set base case
         return [0, 1]
     }
     else if(number === 0){
-        return [0]
-    }
 
+        return [0]
+    };
+
+    // Recursively call function until base case is met
     let fibsArray = fibsRec(number - 1)
+
+    // Add the two previous numbers in the array
     fibsArray[number-1]  = (fibsArray[fibsArray.length - 1] + fibsArray[fibsArray.length - 2])
     return fibsArray
 };
-
-console.log(fibsRec(0));
--- merge.js
+++ merge.js
@@ -1,17 +1,23 @@
 function mergeSort(unsortedArray){
     if(unsortedArray.length <= 1){
+        // Set base case
         return unsortedArray;
     };
+    // Set half of array and recursively call merge sort until base case is met
     let half = Math.floor(unsortedArray.length / 2);
     let leftSide = mergeSort(unsortedArray.slice(0, half));
     let rightSide = mergeSort(unsortedArray.slice(half));
 
+    // Merge the two sorted arrays and return sorted array
     return merge(leftSide, rightSide)
 };
 
 function merge(leftSide, rightSide){
+
+    // Initialize array
     let sortedArray = []
 
+    // Merge the two arrays in order
     while(leftSide.length && rightSide.length){
         if(leftSide[0] < rightSide[0]){
             sortedArray.push(leftSide.shift())
@@ -21,13 +27,16 @@ function merge(leftSide, rightSide){
             }
         }
 
+    // If left array isn't empty, add all the values onto the sorted array
     while(leftSide.length){
         sortedArray.push(leftSide.shift());
     };
+
+    // If the right array isn't empty, add all values onto the sorted array
     while(rightSide.length){
         sortedArray.push(rightSide.shift());
     }
+
+    // Return sorted array
     return sortedArray
 };
-
-console.log(mergeSort([27,15,23,12,0,8,19]));
